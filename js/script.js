cef57d41783dfb1583523a8d12d970b1778ade7f
Emanuele213 <vittorius213@gmail.com>
2022-11-22 15:07:59 +0100

    completed exercise with bonus

---
+++ js/script.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#app',
+    data: {
+        arrEmail: [],
+    },
+    methods: {
+        
+    },
+    computed: {
+        showEmail() {
+			this.arrEmail = [];
+			for (let i = 0; i < 10; i++) {
+				axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(emailResponse => {
+					this.arrEmail.push(emailResponse.data.response);
+				})
+			}
+		},
+    }
+});
